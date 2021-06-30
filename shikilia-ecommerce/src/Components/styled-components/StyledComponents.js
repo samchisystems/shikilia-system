1b693b07d127695e9066bc5d2b1ed082e2ae7b8f
GithuiVictor <victorgithuim@gmail.com>
2021-06-30 22:59:54 +0300

    feat: searchBar & product category

--- shikilia-ecommerce/src/Components/styled-components/StyledComponents.js
+++ shikilia-ecommerce/src/Components/styled-components/StyledComponents.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 import { Col, Row, Button, Card } from 'react-bootstrap'
+import TextField from '@material-ui/core/TextField';
 
 
 export const Section = styled.section`
     background: ${({backgroundColor}) => (
-        backgroundColor === "Default" ? "#172b4d" : 'transparent'
+        backgroundColor === "Default" ? "#172b4d" : 'transparent' &&
+        backgroundColor === "primary" ? "#5e72e4" : 'transparent'
     )};
 `;
 
@@ -17,9 +19,16 @@ export const Container = styled.div`
         justifyContent === "end" ? "flex-end" : "center" 
     )};
     margin: ${({margin}) => (
-        margin === "0-5-0-0" ? "0rem 10rem 0rem 0rem !important" : "0rem 0rem 0rem 0rem" &&
-        margin === "0-0-2-0" ? "0rem 0rem 2rem rem !important" : "0rem 0rem 0rem 0rem"
+        margin === "0-0-0--6" ? "0rem 0rem 0rem -6rem !important" : "0rem 0rem 0rem 0rem" &&
+        margin === "0-0-2-0" ? "0rem 0rem 2rem 0rem !important" : "0rem 0rem 0rem 0rem" &&
+        margin === "2-0-0-0" ? "2rem 0rem 0rem 0rem !important" : "0rem 0rem 0rem 0rem"
     )};
+    background: ${({backgroundColor}) => (
+        backgroundColor === "Default" ? "#172b4d" : 'transparent' &&
+        backgroundColor === "primary" ? "#5e72e4" : 'transparent'
+    )};
+    width: 100% !important;
+    
 `;
 
 
@@ -39,17 +48,28 @@ export const Placeholder = styled.p``;
 
 
 export const TargetLink = styled.a`
+    display: ${({display}) => (
+        display === "flex" ? "flex" : "block" 
+    )};
     text-decoration: none !important;
     color: white;
     margin: ${({margin}) => (
-        margin === "2-2-2-0" ? "2rem 2rem 2rem 0rem !important" : "0rem 0rem 0rem 0rem"
+        margin === "2-2-2.5-0" ? "2rem 2rem 1rem 0rem !important" : "0rem 0rem 0rem 0rem" &&
+        margin === "2-0-0-0" ? "1.5rem 0rem 0rem 0rem !important" : "0rem 0rem 0rem 0rem" &&
+        margin === "2-2-0-0" ? "2rem 2rem 0rem 2rem !important" : "0rem 0rem 0rem 0rem" &&
+        margin === "1-2-0-1" ? "1rem 2rem 1rem 2rem !important" : "0rem 0rem 0rem 0rem"
+    )};
+    border-left: ${({border}) => (
+        border ? "1px solid white" : "none"
     )};
     cursor: pointer !important;
     transition: all 0.3s;
    
     &:hover {
         text-decoration-none !important;
-        color: #5e72e4;
+        color: ${({hoverColor}) => (
+            hoverColor === "Default" ? "#172b4d" : "#5e72e4"
+        )};
     }
 `;
 
@@ -72,12 +92,33 @@ export const Column = styled(Col)`
         justifyContent === "end" ? "flex-end" : "center" 
     )};
     margin: ${({margin}) => (
-        margin === "0-0-2-0" ? "0rem 0rem 2rem rem !important" : "0rem 0rem 0rem 0rem"
+        margin === "0-0-0-0" ? "0rem 0rem -5rem 0rem !important" : "0rem 0rem 0rem 0rem"
     )};
+    
 `;
 
 
-export const Buttons = styled(Button)``;
+export const Buttons = styled(Button)`
+    border: none;
+    background: ${({background}) => (
+        background === "transparent" ? "transparent" : "transparent"
+    )};
+    transition: .3s all;
+
+    &:hover {
+        color: ${({hoverColor}) => (
+            hoverColor === "primary" ? "#5e72e4;" : "#white !important"
+        )};
+        background: ${({background}) => (
+            background === "primary" ? "#172b4d" : "transparent" 
+        )};
+        text-decoration: none;
+        transform: scale(1.20) !important;
+    }
+`;
 
 
 export const Cards = styled(Card)``;
+
+
+export const InputField = styled(TextField)``;

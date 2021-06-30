e60c944825257b80e2156eaf20a1def56d658335
GithuiVictor <victorgithuim@gmail.com>
2021-06-30 08:43:23 +0300

    Feat: Navigation

---
+++ shikilia-ecommerce/src/Data/menuData.js
@@ -0,0 +1,5 @@
+export const topMenuData = [
+    {id: 1, icon: 'FavoriteIcon', title: 'Wish List', border: false},
+    {id: 2, icon: 'CheckIcon', title: 'Checkout', border: true},
+    {id: 3, icon: 'PersonIcon', title: 'Sign In', border: true}
+]

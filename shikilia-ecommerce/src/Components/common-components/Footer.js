a19daa6fc22e9cc3ac1943884b04df44cda25201
GithuiVictor <victorgithuim@gmail.com>
2021-07-09 09:53:47 +0300

    Add footer

---
+++ shikilia-ecommerce/src/Components/common-components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section } from '../styled-components/StyledComponents';
+
+function Footer() {
+    return (
+        <Section>
+            
+        </Section>
+    )
+}
+
+export default Footer
